65307dac5ac0d378b6f846dd0f0267731ab8b63f
093171729 <github20250205@duck.com>
2025-08-31 17:53:32 -0700

    Add Custom 15m Alert sound entry

--- extensions/timers/src/backend/soundData.ts
+++ extensions/timers/src/backend/soundData.ts
@@ -1,41 +1,46 @@
-import { SoundData } from "./types";
-import { Icon } from "@raycast/api";
+import {SoundData} from "./types";
+import {Icon} from "@raycast/api";
 
 const soundData: SoundData[] = [
-  {
-    title: "Alarm Clock",
-    icon: Icon.Alarm,
-    value: "alarmClock.wav",
-  },
-  {
-    title: "Dismembered Woodpecker",
-    icon: Icon.Bird,
-    value: "dismemberedWoodpecker.wav",
-  },
-  {
-    title: "Flute Riff",
-    icon: Icon.Music,
-    value: "fluteRiff.wav",
-  },
-  {
-    title: "Level Up",
-    icon: Icon.Trophy,
-    value: "levelUp.wav",
-  },
-  {
-    title: "Piano Chime",
-    icon: Icon.Music,
-    value: "pianoChime.wav",
-  },
-  {
-    title: "Terminator",
-    icon: Icon.BarCode,
-    value: "terminator.wav",
-  },
-  {
-    title: "Speak Timer Name",
-    icon: Icon.Person,
-    value: "speak_timer_name",
-  },
+    {
+        title: "Alarm Clock",
+        icon: Icon.Alarm,
+        value: "alarmClock.wav",
+    },
+    {
+        title: "Dismembered Woodpecker",
+        icon: Icon.Bird,
+        value: "dismemberedWoodpecker.wav",
+    },
+    {
+        title: "Flute Riff",
+        icon: Icon.Music,
+        value: "fluteRiff.wav",
+    },
+    {
+        title: "Level Up",
+        icon: Icon.Trophy,
+        value: "levelUp.wav",
+    },
+    {
+        title: "Piano Chime",
+        icon: Icon.Music,
+        value: "pianoChime.wav",
+    },
+    {
+        title: "Terminator",
+        icon: Icon.BarCode,
+        value: "terminator.wav",
+    },
+    {
+        title: "Speak Timer Name",
+        icon: Icon.Person,
+        value: "speak_timer_name",
+    },
+    {
+        title: "Custom 15m Alert",
+        icon: Icon.Bell,
+        value: "custom_15m_wav",
+    },
 ];
-export { soundData };
+export {soundData};
